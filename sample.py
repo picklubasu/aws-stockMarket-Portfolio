17d2279ec6151e51f27cb7d7d1d14c61e27c8ae4
picklubasu <basu.saptarsi91@gmail.com>
2023-10-18 23:30:42 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,14 +1,8 @@
 import requests
 import json
 
-text = {
-    'Accept':'*/*',
-    'Accept-Encoding':'gzip, deflate, br',
-    'Accept-Language':'en-US,en;q=0.9',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
-}
+request_header = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' }
+request_url = 'https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20TOTAL%20MARKET'
 
-r = requests.get('https://www.nseindia.com/', headers=text,allow_redirects=False)
-
-for c in r.cookies:
-    print(c.name +"==>>", c.value)
+response = requests.get(url = request_url, headers = request_header)
+response.json()
